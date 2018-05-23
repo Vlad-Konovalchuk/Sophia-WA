6673700da284cbc0fb484dd37a82295aeedd89a5
Vlad Konovalchuk <berlitio600@gmail.com>
2018-05-24 01:56:57 +0300

    Creat Dropdown Menu to change currency

---
+++ src/index/scripts/dropDown.js
@@ -0,0 +1,48 @@
+function dropDownMenu(el) {
+    const Eltitle = el.querySelector('.dropdown__title');
+    let stateDropDown = el.classList.contains('opened');
+
+    function close(text) {
+        Eltitle.textContent = text;
+        stateDropDown = false;
+        el.classList.remove('opened');
+        document.removeEventListener('click', close);
+    }
+
+    function open() {
+        stateDropDown = true;
+        el.classList.add('opened');
+        document.addEventListener('click',  close);
+
+    }
+
+    function toggle(e){
+        e.stopPropagation();
+
+        if (stateDropDown) {
+            close();
+            setContent(e.target.textContent)
+        } else {
+            open();
+        }
+    }
+    
+    function setContent(text) {
+        Eltitle.textContent = text;
+    }
+
+    el.addEventListener('click', (e) => {
+        toggle(e)
+    });
+    el.addEventListener('keypress', (e) => {
+        if (e.keyCode === 13) {
+            toggle(e)
+        }
+    });
+
+
+
+}
+
+
+dropDownMenu(document.querySelector('#dropDownMenu'));
